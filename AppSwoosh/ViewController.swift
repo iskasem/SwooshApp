540f9d5f9b4c14b03071c66199aebb1d1ec7d841
Islam Kasem <is_kasem@yahoo.com>
2019-01-28 18:44:08 +0300

    UI stack view and 2 new screens

--- AppSwoosh/ViewController.swift
+++ AppSwoosh/ViewController.swift
@@ -16,13 +16,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    
+    /* 
         swoosh.frame = CGRect(x: view.frame.size .width/2 - swoosh.frame.size .width / 2, y: 50, width: swoosh.frame.size .width, height: swoosh.frame.size.height)
         bgimage.frame = view.frame
         
     
     }
 
-
+*/
+}
 }
 
