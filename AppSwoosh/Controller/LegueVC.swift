b1d32634ecb367910f57e3b21a8b9130d701c98f
Islam Kasem <is_kasem@yahoo.com>
2019-01-29 07:59:46 +0300

    new view controllers

---
+++ AppSwoosh/Controller/LegueVC.swift
@@ -0,0 +1,34 @@
+//
+//  LegueVCViewController.swift
+//  AppSwoosh
+//
+//  Created by Islam Kasem on 29/01/2019.
+//  Copyright © 2019 Islam Kasem. All rights reserved.
+//
+
+import UIKit
+
+class LegueVC: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func onNextTapped(_ sender: Any) {
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
