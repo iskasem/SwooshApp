e5daca3e6cda2f399ea9e0f14624b3d04d1635bf
Islam Kasem <is_kasem@yahoo.com>
2019-01-29 07:25:33 +0300

    segues exit with short code

--- AppSwoosh/ViewController.swift
+++ AppSwoosh/ViewController.swift
@@ -19,11 +19,14 @@ class ViewController: UIViewController {
     /* 
         swoosh.frame = CGRect(x: view.frame.size .width/2 - swoosh.frame.size .width / 2, y: 50, width: swoosh.frame.size .width, height: swoosh.frame.size.height)
         bgimage.frame = view.frame
-        
     
     }
-
 */
-}
+    
+    }
+    @IBAction func unwindFromSkillVC (unwindSwgue:UIStoryboardSegue){
+        
+    }
+    
 }
 
