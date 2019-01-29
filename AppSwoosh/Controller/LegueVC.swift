3b4f0506881fdd79ea5a6165b035015dafdd53f9
Islam Kasem <is_kasem@yahoo.com>
2019-01-29 08:10:21 +0300

    Segues programatically

--- AppSwoosh/Controller/LegueVC.swift
+++ AppSwoosh/Controller/LegueVC.swift
@@ -17,7 +17,7 @@ class LegueVC: UIViewController {
         // Do any additional setup after loading the view.
     }
     @IBAction func onNextTapped(_ sender: Any) {
-        
+        performSegue(withIdentifier: "skillVCSegue", sender: self)
     }
     
 
